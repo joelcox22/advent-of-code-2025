ea996b7fda847fecbd39a444764065200744a687
Joel Cox <joel@pinecodes.com>
2025-12-05 04:51:39 +0000

    day 3

---
+++ day-3/part1.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S deno --allow-read
+
+const input = await Deno.readTextFile((await Deno.args)[0] ?? "sample.txt");
+
+const banks = input.trim().split("\n").map((bank) =>
+  bank.split("").map((v) => parseInt(v, 10))
+);
+
+function find(bank: number[], repeat = 0): number {
+  console.log('checking bank', bank.join(''));
+  let max = 0;
+  for (let i=9; i>=0; i--) {
+    const index = bank.indexOf(i);
+    if (index >= 0) {
+      console.log('found', i, 'at index', index);
+      const value = bank[index];
+      if (repeat > 0 && index < bank.length-1) {
+        const newBank = bank.slice(index+1);
+        max = Math.max(max, parseInt(value.toString() + find(newBank, repeat-1), 10));
+      } else {
+        max = Math.max(max, value);
+      }
+    }
+  }
+  return max;
+}
+
+const joltages = banks.map((bank) => {
+  return find(bank, 1);
+});
+
+console.log('joltages:', joltages);
+
+const sum = joltages.reduce((acc, v) => {
+  acc += v;
+  return acc;
+}, 0);
+
+console.log('Total maximum joltage:', sum);
---
+++ day-3/part2.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S deno --allow-read
+
+const input = await Deno.readTextFile((await Deno.args)[0] ?? "sample.txt");
+
+const banks = input.trim().split("\n").map((bank) =>
+  bank.split("").map((v) => parseInt(v, 10))
+);
+
+const digits = 12;
+
+const joltages = banks.map((bank) => {
+  const indexes = new Array(digits).fill(0).map((_, i) => bank.length - i - 1).reverse();
+  next: for (let i=0; i<indexes.length; i++) {
+    for (let j=9; j>=bank[indexes[i]]; j--) {
+      for (let k = (i === 0 ? 0 : indexes[i-1]+1); k<indexes[i]; k++) {
+        if (bank[k] === j) {
+          indexes[i] = k;
+          continue next;
+        }
+      }
+    }
+  }
+  return parseInt(indexes.map((index) => bank[index]).join(''), 10);
+});
+
+const sum = joltages.reduce((acc, v) => {
+  acc += v;
+  return acc;
+}, 0);
+
+console.log('Total maximum joltage:', sum);
